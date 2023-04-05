1c96364ef5950d836f22ac15e7d7a3caddced3f5
Ondřej Gardavský <ogardavsky@gmail.com>
2023-04-05 18:33:25 +0200

    copy password and userName

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,7 +29,7 @@ class HomeScreen extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => ViewScreen(),
+                    builder: (context) => ViewScreen( passwordRecord: passwordRecords[index]),
                   ),
                 );
               },
--- lib/screens/view_screen.dart
+++ lib/screens/view_screen.dart
@@ -1,20 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ViewScreen extends StatelessWidget {
-  // const HomeScreen({super.key});
+import '../models/password_record.dart';
 
+class ViewScreen extends StatefulWidget {
+  final PasswordRecord? passwordRecord;
 
+  ViewScreen({this.passwordRecord});
+
+  @override
+  _ViewScreenState createState() => _ViewScreenState();
+}
+
+class _ViewScreenState extends State<ViewScreen> {
+  bool _showPassword = false;
 
   @override
   Widget build(BuildContext context) {
+    if (widget.passwordRecord == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("View Password Record"),
+        ),
+        body: Center(
+          child: Text("No password record found."),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('view screen'),
+        title: Text(widget.passwordRecord!.name),
       ),
-      body: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(fontSize: 24.0),
+      body: Padding(
+        padding: EdgeInsets.all(0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              subtitle: Text("Website Name"),
+              title: Text(widget.passwordRecord!.name),
+            ),
+            ListTile(
+              subtitle: Text("Username"),
+              title: Text(widget.passwordRecord!.userName),
+              trailing: IconButton(
+                icon: Icon(Icons.copy ),
+                onPressed: () async {
+                  await Clipboard.setData(ClipboardData(text: widget.passwordRecord!.userName));
+                },
+              ),
+            ),
+            ListTile(
+              subtitle: Text("Password"),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      _showPassword ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _showPassword = !_showPassword;
+                      });
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.copy ),
+                    onPressed: () async {
+                      await Clipboard.setData(ClipboardData(text: widget.passwordRecord!.password));
+                    },
+                  ),
+                ],
+              ),
+              title: _showPassword
+                  ? Text(widget.passwordRecord!.password)
+                  : Text("********"),
+            ),
+          ],
         ),
       ),
     );
